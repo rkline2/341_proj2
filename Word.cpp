b02cb2a1ca1dfade641737e55ef8763ef4dcfd81
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-06-16 16:24:04 -0400

    version 1.0

---
+++ Word.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Word.h"
+using namespace std;
+
+Word::Word() {}
+
+Word::Word(string, int) {}
+
+void Word::CountWord(int lineNum) {}
+
+string Word::GetWord() {}
+
+bool Word::operator<(Word& RHS) {}
+
+Word& Word::operator=(Word& RHS) {}
+
+ostream& operator<<(ostream out, Word& source) {}

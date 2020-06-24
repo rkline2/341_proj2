fd62125658b0ef4ca06869e13c5479f1c02eaca1
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-06-24 15:08:00 -0400

    version 1.1

--- Indexer.h
+++ Indexer.h
@@ -1,7 +1,14 @@
 #ifndef INDEXER_H
 #define INDEXER_H
+
+#include <iostream>
+#include <fstream>
 #include <string>
 #include "BinarySearchTree.h"
+#include "Exceptions.h"
+using namespace std;
+
+
 
 class Indexer {
 public:
@@ -11,14 +18,27 @@ public:
 	// Overloaded Constructor 
 	Indexer(string filterFile, string dataFile);
 
+
 	// Do Index 
 	void DoIndex();
 
+	bool FileExists(char*);
 
+	BinarySearchTree<Word>& FileFilterReader(string);
+
+	BinarySearchTree<Word>& FileWordReader(string);
+
+	void String_To_Char(string, char []);
+
+	void Export(string);
 
 private:
-	BinarySearchTree<Word> m_filteredBST;
-	BinarySearchTree<Word> m_indexBST;
+
+	void FilterLine(string&, int&, BinarySearchTree<Word>&);
+	void InsertWord(string& word, const int& lineNumber, BinarySearchTree<Word>& WordTree);
+
+	BinarySearchTree<Word>* m_filteredBST;
+	BinarySearchTree<Word>* m_indexBST;
 	string m_filterFile = "";
 	string m_dataFile = "";
 

9cdf164292256ebd13df9b8840bceaa24254524b
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-06-25 16:32:59 -0400

    version 1.0

---
+++ TestIndexer.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include "Indexer.h"
+#include "Word.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string inFilterFile = ""; //Filter file.
+    string inDataFile = ""; //Data file.
+    string usrResponse = "";
+
+    //Do we have the right number of aruguments?  We will validate those
+    //arguments later.
+    if (argc != 2) {
+        cout << "Usage:  ./Proj2.out filter.file data.file\n" << endl;
+        cout << "filter.file - The file that contains the list of words";
+        cout << " not to be included." << endl;
+        cout << "data.file - The file that contains the text";
+        cout << " to be indexed.\n" << endl;
+
+        //This is the main driver.
+    }
+
+    else {
+        const string FILTER_CPY = argv[1]; inFilterFile = argv[1];
+        
+        cout << "Welcome to Index Test" << endl;
+        
+        // Test 1 Indexing without a filter BST 
+        cout << "Test 1: Indexing with and without a filter file" << endl;
+        cout << "Enter any value to continue: "; cin >> usrResponse;
+
+        try {
+            cout << "Testing without a filter file" << endl;
+            cout << "Enter any value to continue: "; cin >> usrResponse;
+
+            inFilterFile = ""; inDataFile = "TestInput1.txt";
+            Indexer* index1 = new Indexer(inFilterFile, inDataFile);
+            index1->DoIndex();
+            delete index1;
+            index1 = nullptr;
+
+            cout << "Testing with a filter file" << endl;
+            cout << "Enter any value to continue: "; cin >> usrResponse;
+
+            inFilterFile = FILTER_CPY; inDataFile = "TestInput1.txt";
+            index1 = new Indexer(inFilterFile, inDataFile);
+            index1->DoIndex();
+            delete index1;
+            index1 = nullptr;
+        }
+
+        catch (Exceptions& e) {
+            cout << "Error " << e.GetMessage() << endl;
+        }
+
+        // Test 2 Indexing without a Word BST (Expect an error) 
+        cout << "Test 2: Indexing with and without a word file" << endl;
+        cout << "Enter any value to continue: "; cin >> usrResponse;
+        try {
+            cout << "Testing without TestInput1.txt (expect an error)" << endl;
+            cout << "Enter any value to continue: "; cin >> usrResponse;
+            inFilterFile = ""; inDataFile = "";
+            Indexer* index2 = new Indexer(inFilterFile, inDataFile);
+            index2->DoIndex();
+            delete index2;
+            index2 = nullptr;
+        }
+        catch (Exceptions& e) {
+            cout << "Error " << e.GetMessage() << endl;
+        }
+        cout << "Testing with TestInput1.txt" << endl;
+        cout << "Enter any value to continue: "; cin >> usrResponse;
+        try {
+            inFilterFile = FILTER_CPY; inDataFile = "TestInput1.txt";
+            Indexer* index3 = new Indexer(inFilterFile, inDataFile);
+            index3->DoIndex();
+            delete index3;
+            index3 = nullptr;
+        }
+
+        catch (Exceptions& e) {
+            cout << "Error " << e.GetMessage() << endl;
+        }
+
+        // Test 3 Final 100m
+        cout << "Test 3: Final Test" << endl;
+        cout << "Indexing 3 Test .txt files given" << endl;
+        cout << "Enter any value to continue: "; cin >> usrResponse;
+
+
+
+        /*************************************************************/
+        try {
+            inDataFile = "TestInput2.txt";
+            Indexer* index1 = new Indexer(inFilterFile, inDataFile);
+            index1->DoIndex();
+            delete index1;
+            index1 = NULL;
+        }
+        //Something went wrong.
+        catch (Exceptions& cException) {
+            cout << "EXCEPTION: " << cException.GetMessage() << endl;
+            return 1;
+        }
+        /*************************************************************/
+
+
+
+        /*************************************************************/
+        try {
+            inDataFile = "TestInput3.txt";
+            Indexer* index2 = new Indexer(inFilterFile, inDataFile);
+            index2->DoIndex();
+            delete index2;
+            index2 = NULL;
+        }
+        //Something went wrong.
+        catch (Exceptions& cException) {
+            cout << "EXCEPTION: " << cException.GetMessage() << endl;
+            return 1;
+        }
+        /*************************************************************/
+
+
+
+        /*************************************************************/
+        try {
+            inDataFile = "TestInput4.txt";
+            Indexer* index3 = new Indexer(inFilterFile, inDataFile);
+            index3->DoIndex();
+            delete index3;
+            index3 = NULL;
+        }
+        //Something went wrong.
+        catch (Exceptions& cException) {
+            cout << "EXCEPTION: " << cException.GetMessage() << endl;
+            return 1;
+        }
+        /*************************************************************/
+
+    }
+
+    //Uncomment for number of seconds running.
+    cout << (clock() / (float)CLOCKS_PER_SEC) << endl;
+
+    return 0;
+}
